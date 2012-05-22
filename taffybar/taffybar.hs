6582af2b2c095cf39ee97dbaa54d8f4d982f8f32
Alexey Kotlyarov <koterpillar@gmail.com>
2012-05-22 20:57:38 +1000

    Better styles.

--- taffybar/taffybar.hs
+++ taffybar/taffybar.hs
@@ -4,15 +4,32 @@ import System.Taffybar.FreedesktopNotifications
 import System.Taffybar.SimpleClock
 import System.Taffybar.Systray
 import System.Taffybar.XMonadLog
-import System.Taffybar.Widgets.PollingGraph
+import System.Taffybar.Widgets.PollingBar
+
 import System.Information.CPU
+
+rgb :: Fractional a => (a, a, a) -> (a, a, a)
+rgb (x, y, z) = (norm x, norm y, norm z)
+                where norm a = a / 255
+
+menuColor = rgb (223, 215, 207)
+widgetColor = rgb (242, 241, 240)
+
+batteryConfig = defaultBatteryConfig { barColor = batteryColor
+                                     , barBackgroundColor = menuColor
+                                     , barBorderColor = (0, 0, 0)
+                                     , barWidth = 14
+                                     } where batteryColor pct
+                                                | pct < 0.1 = (1, 0, 0)
+                                                | pct < 0.9 = (0.1, 0.1, 0.1)
+                                                | otherwise = (0, 0, 0)
 
 main = do
   let clock = textClockNew Nothing "%a %b %_d %H:%M" 1
       log = xmonadLogNew
       tray = systrayNew
       note = notifyAreaNew defaultNotificationConfig
-      battery = batteryBarNew defaultBatteryConfig 10
+      battery = batteryBarNew batteryConfig 10
   defaultTaffybar defaultTaffybarConfig { startWidgets = [ log, note ]
                                         , endWidgets = [ tray, clock, battery ]
                                         }
