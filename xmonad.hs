021a7f2ab5d16528e136867bb59517c19beac989
Alexey Kotlyarov <koterpillar@gmail.com>
2012-05-26 11:12:12 +1000

    Browser media key launches browser.

--- xmonad.hs
+++ xmonad.hs
@@ -41,6 +41,7 @@ main = do
         , ((mod1Mask .|. shiftMask, xK_Tab   ), windows W.focusUp  ) -- Alt-Shift-Tab
         , ((0                     , xF86XK_PowerOff), spawn "gnome-session-quit --power-off")
         , ((mod4Mask              , xK_Return), spawn $ XMonad.terminal defaultConfig)
+        , ((0                     , xF86XK_HomePage), spawn "x-www-browser" )
         --, ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
         --, ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
         --, ((0, xK_Print), spawn "scrot")
