675efeb8da3c0d227bf5910b5f62561c28eb2f8b
Alexey Kotlyarov <koterpillar@gmail.com>
2012-05-26 11:11:47 +1000

    Terminal by Win-Enter.

--- xmonad.hs
+++ xmonad.hs
@@ -40,6 +40,7 @@ main = do
         [ ((mod1Mask,               xK_Tab   ), windows W.focusDown) -- Alt-Tab to switch windows
         , ((mod1Mask .|. shiftMask, xK_Tab   ), windows W.focusUp  ) -- Alt-Shift-Tab
         , ((0                     , xF86XK_PowerOff), spawn "gnome-session-quit --power-off")
+        , ((mod4Mask              , xK_Return), spawn $ XMonad.terminal defaultConfig)
         --, ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
         --, ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
         --, ((0, xK_Print), spawn "scrot")
