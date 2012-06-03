d4498824258f0c6af8dd1e95feefe45820f342d1
Alexey Kotlyarov <koterpillar@gmail.com>
2012-06-03 21:37:59 +1000

    Win-B to toggle status bar; avoid conflict with builtin Win-T.

--- xmonad.hs
+++ xmonad.hs
@@ -94,7 +94,7 @@ main = do
         , ((0                     , xF86XK_PowerOff), spawn "gnome-session-quit --power-off")
         , ((mod4Mask              , xK_Return), spawn $ XMonad.terminal defaultConfig)
         , ((0                     , xF86XK_HomePage), spawn "x-www-browser" )
-        , ((mod4Mask              , xK_t     ), sendMessage ToggleStruts)
+        , ((mod4Mask              , xK_b     ), sendMessage ToggleStruts)
         --, ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
         --, ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
         --, ((0, xK_Print), spawn "scrot")
