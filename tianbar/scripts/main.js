d735bcf686c139012b7ecb763313add624c4c280
Alexey Kotlyarov <a@koterpillar.com>
2013-07-20 14:06:46 +1000

    Static background for Tianbar

--- tianbar/scripts/main.js
+++ tianbar/scripts/main.js
@@ -12,13 +12,6 @@ require(
   ],
   function ($) {
     $(document).ready(function () {
-      $.ajax('gsettings:org.gnome.desktop.background/picture-uri')
-      .success(function (background) {
-        $('body').css('background-image',
-          "url('" + background + "')");
-        $('body').css('background-size',
-          screen.width + "px " + screen.height + "px");
-      });
       $('html').css('height', $(document).height());
     });
   }
