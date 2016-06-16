508e9efc90baeb33405a2ebe776ce249e9bedbae
Alexey Kotlyarov <a@koterpillar.com>
2016-06-16 22:15:29 +1000

    Fix setting 24 hour time format

--- config/tianbar/scripts/main.js
+++ config/tianbar/scripts/main.js
@@ -1,3 +1,4 @@
+/* jshint esversion: 6 */
 require.config({
   paths: {
     'tianbar': 'tianbar:///data/scripts',
@@ -5,6 +6,7 @@ require.config({
     'moment': 'tianbar:///data/scripts/vendor/moment'
   }
 });
+
 require(
   [
     'jquery',
@@ -16,7 +18,14 @@ require(
     if (!moment.localeData(lang)) {
       lang = lang.replace(/-.+/, '');
     }
-    moment.localeData(lang)._longDateFormat.LT = 'HH:mm';
+    const to24hour = (fmt) =>
+      fmt.replace('hh:mm A', 'HH:mm').replace('h:mm A', 'H:mm');
+    const dateFormats = moment.localeData(lang)._longDateFormat;
+    for (const fmt in dateFormats) {
+      if (dateFormats.hasOwnProperty(fmt)) {
+        dateFormats[fmt] = to24hour(dateFormats[fmt]);
+      }
+    }
 
     require([
       'tianbar/time',
