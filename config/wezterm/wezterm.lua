e0f3a4f5b6386ad71b07eb78fb47ff9655cbcaf4
Alexey Kotlyarov <a@koterpillar.com>
2021-04-10 20:47:01 +1000

    More Wezterm configuration

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,4 +1,31 @@
 local wezterm = require 'wezterm';
+
+local TAB_BAR_BACKGROUND = "#dbddd9";
+local TAB_BAR_FOREGROUND = "white";
+local TAB_BAR_CONTENT = "#8e908c";
+local TAB_BAR_HOVER = "#8e8db0";
+local TAB_BAR_ACTIVE = "#5f87af";
+
+function tab_style_left(bg_color)
+  return wezterm.format({
+    {Background = {Color=TAB_BAR_BACKGROUND}},
+    {Foreground = {Color=bg_color}},
+    {Text = utf8.char(0x2590)},
+  }) .. " ";
+end
+function tab_style_right(bg_color)
+  return " " .. wezterm.format({
+    {Background = {Color=TAB_BAR_BACKGROUND}},
+    {Foreground = {Color=bg_color}},
+    {Text = utf8.char(0x258C)},
+  });
+end
+
+local TAB_STYLE_LEFT = tab_style_left(TAB_BAR_CONTENT);
+local TAB_STYLE_RIGHT = tab_style_right(TAB_BAR_CONTENT);
+
+local WINDOW_PADDING = 8;
+
 return {
   -- Appearance
   font = wezterm.font("Fira Mono"),
@@ -14,7 +41,7 @@ return {
       "#4171AE",
       "#8959A8",
       "#3E999F",
-      "#FFFEFE"
+      "#FFFEFE",
     },
     brights = {
       "#000000",
@@ -24,30 +51,59 @@ return {
       "#4170AE",
       "#8958A7",
       "#3D999F",
-      "#FFFEFE"
+      "#FFFEFE",
     },
     tab_bar = {
-      background = "#dbddd9",
+      background = TAB_BAR_BACKGROUND,
       active_tab = {
-        bg_color = "#5f87af",
-        fg_color = "white"
+        bg_color = TAB_BAR_ACTIVE,
+        fg_color = TAB_BAR_FOREGROUND,
       },
       inactive_tab = {
-        bg_color = "#8e908c",
-        fg_color = "white"
-      }
-    }
+        bg_color = TAB_BAR_CONTENT,
+        fg_color = TAB_BAR_FOREGROUND,
+      },
+      inactive_tab_hover = {
+        bg_color = TAB_BAR_HOVER,
+        fg_color = TAB_BAR_FOREGROUND,
+        intensity = "Bold",
+      },
+    },
+    scrollbar_thumb = TAB_BAR_CONTENT,
+  },
+  inactive_pane_hsb = {
+    saturation = 0.9,
+    brightness = 0.97,
   },
   window_decorations = "NONE",
   window_padding = {
-    left = 8,
-    right = 8,
-    top = 8,
-    bottom = 8
+    left = WINDOW_PADDING,
+    right = WINDOW_PADDING,
+    top = WINDOW_PADDING,
+    bottom = WINDOW_PADDING,
+  },
+  enable_scroll_bar = true,
+  show_tab_index_in_tab_bar = false,
+  tab_bar_style = {
+    active_tab_left = tab_style_left(TAB_BAR_ACTIVE),
+    active_tab_right = tab_style_right(TAB_BAR_ACTIVE),
+    inactive_tab_left = TAB_STYLE_LEFT,
+    inactive_tab_right = TAB_STYLE_RIGHT,
+    inactive_tab_hover_left = TAB_STYLE_LEFT,
+    inactive_tab_hover_right = TAB_STYLE_RIGHT,
+    new_tab_left = TAB_STYLE_LEFT,
+    new_tab_hover_left = TAB_STYLE_LEFT,
   },
 
   -- Keyboard and mouse
   use_ime = true,
+  keys = {
+    { key = "PageUp", mods = "CTRL", action = wezterm.action{ ActivateTabRelative=-1 } },
+    { key = "PageDown", mods = "CTRL", action = wezterm.action{ ActivateTabRelative=1 } },
+    { key = "E", mods = "CTRL|SHIFT", action = wezterm.action{ SplitVertical={ domain="CurrentPaneDomain" } } },
+    { key = "O", mods = "CTRL|SHIFT", action = wezterm.action{ SplitHorizontal={ domain="CurrentPaneDomain" } } },
+    { key = "H", mods = "SUPER", action = "Nop" },
+  },
 
   -- Behavior
   scrollback_lines = 10000,
