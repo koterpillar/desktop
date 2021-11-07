bbd9d931d49f600121a39932f2fbf7a732d21406
Alexey Kotlyarov <a@koterpillar.com>
2021-11-07 14:42:56 +1100

    Migrate some settings to Lua

---
+++ config/nvim/lua/settings.lua
@@ -0,0 +1,35 @@
+local cmd = vim.cmd
+local opt = vim.opt
+local map = vim.api.nvim_set_keymap
+local default_opts = {noremap = true, silent = true}
+
+opt.colorcolumn = '80'
+opt.number = true
+opt.relativenumber = true
+
+opt.tabstop = 2
+opt.softtabstop = -1
+opt.shiftwidth = 0
+opt.expandtab = true
+cmd [[autocmd FileType python setlocal tabstop=4]]
+
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.undofile = true
+opt.updatetime = 250
+
+opt.wildignore = { "node_modules", "__pycache__", "*.pyc" }
+
+opt.langmap = "ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz"
+map('n', '<leader>ff', '<cmd>Telescope find_files<cr>', default_opts)
+map('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', default_opts)
+map('n', '<leader>fb', '<cmd>Telescope buffers<cr>', default_opts)
+map('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', default_opts)
+
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = "maintained",
+  highlight = {
+    enable = true
+  }
+}
