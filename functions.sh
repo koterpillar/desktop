02e2e41882b9e958314c5c106001f14ce133d471
Alexey Kotlyarov <a@koterpillar.com>
2022-01-17 21:17:26 +1100

    fresh macOS fixes

--- functions.sh
+++ functions.sh
@@ -1,13 +1,11 @@
 if (echo $OSTYPE | grep -q darwin)
 then
-    LN=gln
-    READLINK=greadlink
     OS=macos
+    DIR=$(cd $(dirname $0); pwd -P)
+    PATH="/usr/local/opt/coreutils/libexec/gnubin:/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
 else
-    LN=ln
-    READLINK=readlink
     OS=linux
     OS_DISTRIBUTION=$(cat /etc/os-release | grep '^ID=' | cut -d = -f 2)
+    DIR=$(readlink -f $(dirname $0))
 fi
 
-DIR=$($READLINK -f $(dirname $0))
