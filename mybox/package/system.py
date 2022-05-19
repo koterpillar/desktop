1cd0c432988ca2ab47d751b1846800605dcb7c6b
Alexey Kotlyarov <a@koterpillar.com>
2022-05-19 14:01:57 +1000

    Get versions from Brew

--- mybox/package/system.py
+++ mybox/package/system.py
@@ -1,5 +1,8 @@
+import json
 import shutil
 from abc import ABCMeta, abstractmethod
+from functools import cache
+from typing import Optional
 
 from ..utils import *
 from .base import Package
@@ -10,17 +13,47 @@ class Installer(metaclass=ABCMeta):
     def install(self, *packages: str) -> None:
         pass
 
-    @abstractmethod
+    def installed_version(self, package: str) -> Optional[str]:
+        raise NotImplementedError()
+
     def is_installed(self, package: str) -> bool:
-        pass
+        return self.installed_version(package) is not None
+
+    def latest_version(self, package: str) -> str:
+        return "repository"
 
 
 class Brew(Installer):
     def install(self, *packages: str) -> None:
         run("brew", "install", *packages)
 
-    def is_installed(self, package: str) -> bool:
-        return run_ok("brew", "list", package)
+    @cache
+    def info(self, package: str) -> tuple[str, Optional[str]]:
+        info = json.loads(run_output("brew", "info", "--json=v2", package))
+        if info["casks"]:
+            cask = info["casks"][0]
+            return cask["version"], cask["installed"]
+        if info["formulae"]:
+            formula = info["formulae"][0]
+            try:
+                installed = formula["installed"][0]["version"]
+            except IndexError:
+                installed = None
+            version = formula["versions"]["stable"]
+            revision = formula.get("revision")
+            if revision:
+                version += f"_{revision}"
+            return version, installed
+        raise ValueError(f"Unexpected output from brew: {info}")
+
+    def installed_version(self, package: str) -> Optional[str]:
+        try:
+            return self.info(package)[1]
+        except IndexError:
+            return None
+
+    def latest_version(self, package: str) -> str:
+        return self.info(package)[0]
 
 
 class DNF(Installer):
@@ -64,17 +97,20 @@ class SystemPackage(Package):
         return self._name
 
     def get_remote_version(self) -> str:
-        return "repository"
+        return INSTALLER.latest_version(self.name)
 
     @property
     def local_version(self) -> Optional[str]:
-        if INSTALLER.is_installed(self.name):
-            return "repository"
-        else:
-            return None
+        return INSTALLER.installed_version(self.name)
 
-    def install(self):
-        INSTALLER.install(self.name)
+    def postinstall_linux(self):
         run("sudo", "systemctl", "daemon-reload")
         for service in self.services:
             run("sudo", "systemctl", "enable", service)
+
+    def postinstall_macos(self):
+        pass
+
+    def install(self):
+        INSTALLER.install(self.name)
+        with_os(linux=self.postinstall_linux, macos=self.postinstall_macos)()
