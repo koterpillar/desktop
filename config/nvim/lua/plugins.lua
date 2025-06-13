f8d3398219cd04632e974b1125b63cefba0eeb70
Alexey Kotlyarov <a@koterpillar.com>
2025-06-13 11:58:38 +1000

    Fix rainbow plugin for Vim

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -6,10 +6,6 @@ require("nvim-treesitter.configs").setup {
   ensure_installed = "all",
   highlight = {
     enable = true
-  },
-  rainbow = {
-    enable = true,
-    max_file_lines = 5000,
   }
 }
 
